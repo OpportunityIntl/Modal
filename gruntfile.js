c441425f408863a8495e69d063e393e2e7413a16
Joe Weber <wosephjeber@gmail.com>
2015-07-22 13:38:19 -0500

    Extracting modals to their own repo

---
+++ gruntfile.js
@@ -0,0 +1,68 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    banner: '/* <%= pkg.name %> v<%= pkg.version %> \n' +
+      ' * Compiled <%= grunt.template.today("mm-dd-yyyy") %>\n' +
+      ' * Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author %> \n */\n',
+    
+    sass: {
+      dev: {
+        options: {
+          style: 'expanded'
+        },
+        files: {
+          'dist/OI.modal.css' : 'sass/OI.modal.scss'
+        }
+      },
+      production: {
+        options: {
+          style: 'compact',
+          sourcemap: 'none',
+          banner: '<%= banner %>'
+        },
+        files: {
+          'dist/OI.modal.min.css' : 'sass/OI.modal.scss'
+        }
+      }
+    },
+    
+    uglify: {
+      production: {
+        options: {
+          banner: '<%= banner %>'
+        },
+        files: {
+          'dist/OI.modal.min.js': ['js/OI.modal.js']
+        }
+      }
+    },
+    
+    watch: {
+      default: {
+        files: ['sass/**/*.scss', 'js/**/*.js'],
+        tasks: ['default']
+      },
+      options: {
+        interrupt: false,
+        nospawn: true,
+        event: 'all',
+        interval: 1000,
+        debounceDelay: 1000
+      },
+    }
+    
+  });
+
+  // Load plugins
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+
+  // Default task(s).
+  grunt.registerTask('default', [ 'sass:dev', 'sass:production', 'uglify:production' ] );
+
+};
---
+++ js/OI.modal.js
@@ -0,0 +1,112 @@
+var OI = OI || {};
+
+OI.modal = {
+  overlay: $('.modal-overlay'),
+  init: function() {
+    $('.modal-trigger').each(function() {
+      new Modal($(this));
+    });
+  },
+  
+  show: function(elem) {
+    $('body').addClass('animate-modal');
+    setTimeout(function() {
+      $('body').addClass('show-modal');
+      elem.addClass('show');
+    }, 100);
+    
+    elem.find('.close').bind('click.modal', function() {
+      OI.modal.hide(elem);
+    });
+    
+    $('#modals').bind('click.modal', function() {
+      OI.modal.hide(elem);
+    });
+    
+    elem.bind('click.modal', function(e) {
+      e.stopPropagation();
+    });
+    
+    $(document).bind('keyup.modal', function (e) {
+      if (e.keyCode == '27') {
+        OI.modal.hide(elem);
+      }
+    });
+    
+    return false;
+  },
+  
+  hide: function(elem) {
+    elem.find('.close').unbind('click.modal');
+    $('#modals').unbind('click.modal');
+    elem.unbind('click.modal');
+    $(document).unbind('keyup.modal');
+    
+    $('body').removeClass('show-modal');
+    elem.removeClass('show');
+    setTimeout(function() {
+      $('body').removeClass('animate-modal');
+      if (elem.data('destroyOnClose')) {
+        elem.remove();
+      }
+    }, 500);
+  },
+  
+  new: function(content, classes) {
+    var modal = $('<div>', {class: 'modal'});
+    modal.append($('<button>', {type: 'button', class: 'reset icon-cross close'}));
+    modal.append($('<div>', {class: 'modal-content'}));
+    if (classes) modal.addClass(classes);
+    modal.find('.modal-content').append(content);
+    $('#modals').append(modal);
+    modal.data('destroyOnClose', true);
+    return modal;
+  }
+};
+
+var Modal = function(triggerClass, options) {
+  var elem = $(triggerClass);
+  var modal;
+
+  options = $.extend({
+    classes: elem.data('modal-classes') || null,
+    content: null
+  }, options);
+  
+  function isUrl() {
+    return /^https?:\/\//.test(elem.data('modal'));
+  }
+  
+  function isVideo() {
+    return /^https?:\/\/(www\.youtube|player\.vimeo)/.test(elem.data('modal'));
+  }
+  
+  function alreadyExists() {
+    return ($(elem.data('modal')).length > 0);
+  }
+  
+  elem.click(function() {
+    if (isUrl()) {
+      var iframe;
+      if (isVideo()) {
+        console.log('Video');
+        iframe = $('<div>', {class: 'flex-video'});
+        iframe.append($('<iframe>', {src: elem.data('modal'), frameborder: 0}));
+      } else {
+        console.log('Normal url');
+        iframe = $('<iframe>', {src: elem.data('modal'), frameborder: 0});
+      }
+      modal = OI.modal.new(iframe, options.classes);
+    } else {
+      if (alreadyExists()) {
+        modal = $(elem.data('modal'));
+      } else {
+        modal = OI.modal.new(options.content, options.classes);
+      }
+    }
+    
+    OI.modal.show(modal);
+  });
+};
+
+OI.modal.init();
