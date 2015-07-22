803438ca65a9a42e022de3d5960f179b17f77960
Joe Weber <wosephjeber@gmail.com>
2015-07-22 15:34:31 -0500

    Adding some comments to document code

--- js/OI.modal.js
+++ js/OI.modal.js
@@ -2,33 +2,41 @@ var OI = OI || {};
 
 OI.modal = {
   init: function() {
+    // If #modals div doesn't already exist, create it
     if ($('#modals').length === 0) {
       $('body').append($('<div>', {id: "modals"}));
     }
+    
+    // automatically set up triggers for elements with data-modal attribute
     $('[data-modal]').each(function() {
       new Modal($(this));
     });
   },
   
   show: function(elem) {
+    // add class to body to start modal overlay animation
     $('body').addClass('animate-modal');
+    
+    // start modal animation shortly after overlay animation
     setTimeout(function() {
       $('body').addClass('show-modal');
       elem.addClass('show');
     }, 100);
     
+    // close modal when close button is clicked
     elem.find('.close').bind('click.modal', function() {
       OI.modal.hide(elem);
     });
     
+    // close modal when user clicks anywhere outside of modal
     $('#modals').bind('click.modal', function() {
       OI.modal.hide(elem);
     });
-    
     elem.bind('click.modal', function(e) {
-      e.stopPropagation();
+      e.stopPropagation(); // this prevents a click on the actual modal from triggering close
     });
     
+    // close modal with escape key
     $(document).bind('keyup.modal', function (e) {
       if (e.keyCode == '27') {
         OI.modal.hide(elem);
@@ -39,15 +47,21 @@ OI.modal = {
   },
   
   hide: function(elem) {
+    // unbind event handlers that close the modal
     elem.find('.close').unbind('click.modal');
     $('#modals').unbind('click.modal');
     elem.unbind('click.modal');
     $(document).unbind('keyup.modal');
     
+    // start close animation on modal
     $('body').removeClass('show-modal');
     elem.removeClass('show');
+    
+    // start close animation on modal overlay shortly afterward
     setTimeout(function() {
       $('body').removeClass('animate-modal');
+      
+      // if element was created dynamically, remove it on close
       if (elem.data('destroyOnClose')) {
         elem.remove();
       }
